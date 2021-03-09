c3012450dad469d4c785933cebe53ae5983f8061
Aaron Saksik <aaron.saksik@ynov.com>
2021-03-09 10:17:45 +0100

    controlleur controller

--- src/routes/orders.route.js
+++ src/routes/orders.route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const order = require('../controllers/Orders.controlleur');
+const order = require('../controllers/Orders.controller');
 
 router.post('/Orders', order.create);
 router.get('/Orders', order.read);
--- src/routes/products.route.js
+++ src/routes/products.route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const product = require('../controllers/products.controlleur');
+const product = require('../controllers/products.controller');
 
 router.post('/products', product.create);
 router.get('/products', product.read);
